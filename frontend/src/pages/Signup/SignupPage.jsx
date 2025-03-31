35be112ed850d0f57659c16b1a5c2bf654a9dcfa
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2025-03-31 17:45:34 +0100

    add signup error message

--- frontend/src/pages/Signup/SignupPage.jsx
+++ frontend/src/pages/Signup/SignupPage.jsx
@@ -7,7 +7,7 @@ export function SignupPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
+  const [error, setError] = useState(false);
   async function handleSubmit(event) {
     event.preventDefault();
     try {
@@ -15,6 +15,7 @@ export function SignupPage() {
       navigate("/login");
     } catch (err) {
       console.error(err);
+      setError(true);
       navigate("/signup");
     }
   }
@@ -30,24 +31,37 @@ export function SignupPage() {
   return (
     <>
       <h2>Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
+      <form onSubmit={handleSubmit} className="auth-form">
+        <label htmlFor="email">Email</label>
         <input
           id="email"
           type="text"
           value={email}
           onChange={handleEmailChange}
+          required
         />
-        <label htmlFor="password">Password:</label>
+        <label htmlFor="password">Password</label>
         <input
-          placeholder="Password"
+          placeholder="password"
           id="password"
           type="password"
           value={password}
           onChange={handlePasswordChange}
+          required
         />
         <input role="submit-button" id="submit" type="submit" value="Submit" />
       </form>
+      {error && (
+        <div className="auth-error-msg">
+          <p>Email already exists</p>
+        </div>
+      )}
+      <div>
+        <p>
+          Already have an account?
+          <a href="/login"> Login</a>
+        </p>
+      </div>
     </>
   );
 }
