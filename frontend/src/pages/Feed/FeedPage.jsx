5b9f7b5cb172004d33957186b095e41ce62c9b85
Zameer Mohamed <zameermohamed@hotmail.com>
2025-03-31 12:32:27 +0100

    Initial commit

---
+++ frontend/src/pages/Feed/FeedPage.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { getPosts } from "../../services/posts";
+import Post from "../../components/Post";
+import LogoutButton from "../../components/LogoutButton";
+
+export function FeedPage() {
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const loggedIn = token !== null;
+    if (loggedIn) {
+      getPosts(token)
+        .then((data) => {
+          setPosts(data.posts);
+          localStorage.setItem("token", data.token);
+        })
+        .catch((err) => {
+          console.error(err);
+          navigate("/login");
+        });
+    }
+  }, [navigate]);
+
+  const token = localStorage.getItem("token");
+  if (!token) {
+    navigate("/login");
+    return;
+  }
+
+  return (
+    <>
+      <h2>Posts</h2>
+      <div className="feed" role="feed">
+        {posts.map((post) => (
+          <Post post={post} key={post._id} />
+        ))}
+      </div>
+      <LogoutButton />
+    </>
+  );
+}
