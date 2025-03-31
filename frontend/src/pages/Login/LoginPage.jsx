2b84d61ee130b3c76f4b6058f0f5661d61e8a459
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2025-03-31 17:45:22 +0100

    add login error message

--- frontend/src/pages/Login/LoginPage.jsx
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -7,7 +7,7 @@ export function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
+  const [error, setError] = useState(false);
   async function handleSubmit(event) {
     event.preventDefault();
     try {
@@ -16,6 +16,8 @@ export function LoginPage() {
       navigate("/posts");
     } catch (err) {
       console.error(err);
+      console.log(err.message);
+      setError(true);
       navigate("/login");
     }
   }
@@ -31,23 +33,39 @@ export function LoginPage() {
   return (
     <>
       <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
+      <form onSubmit={handleSubmit} className="auth-form">
+        <label htmlFor="email">Email</label>
         <input
           id="email"
           type="text"
           value={email}
           onChange={handleEmailChange}
+          required
         />
-        <label htmlFor="password">Password:</label>
+        <label htmlFor="password">Password</label>
         <input
           id="password"
           type="password"
+          placeholder="password"
           value={password}
           onChange={handlePasswordChange}
+          required
         />
         <input role="submit-button" id="submit" type="submit" value="Submit" />
       </form>
+
+      {error && (
+        <div className="auth-error-msg">
+          <p>Invalid credentials</p>
+          <p>Please try again</p>
+        </div>
+      )}
+      <div>
+        <p>
+          Don&apos;t have an account?
+          <a href="/signup"> Signup</a>
+        </p>
+      </div>
     </>
   );
 }
