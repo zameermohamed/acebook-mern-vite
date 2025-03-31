059720c87d615e97a9e1c4dc38d681bb6d82befb
Jack Misner <jack.misner@gmail.com>
2025-03-31 21:54:07 +0100

    css fixes for new logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import "../index.css"; // Import the CSS file
 import LogOut from "./Logout";
+import acebook from "../../public/images/acebook.png";
 
 const Header = () => {
   return (
     <>
       <header className="header">
         <Link to="/" className="logo">
-          Acebook
+          <img src={acebook}></img>
         </Link>
         <nav className="nav">
           <Link to="/login" className="nav-link">
