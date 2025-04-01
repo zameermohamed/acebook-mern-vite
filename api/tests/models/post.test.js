edcab668e516dd220ef620801de61ebcd759200e
Amritpal <amritpalchahal@hotmail.co.uk>
2025-04-01 16:35:42 +0100

    Post model test driven

--- api/tests/models/post.test.js
+++ api/tests/models/post.test.js
@@ -1,27 +1,57 @@
 require("../mongodb_helper");
-
+const mongoose = require("mongoose");
 const Post = require("../../models/post");
+const User = require("../../models/user");
+
+const imageURL = "exampleImageURLHere";
 
 describe("Post model", () => {
+  let user;
+  let post;
+
   beforeEach(async () => {
     await Post.deleteMany({});
+    await User.deleteMany({});
+
+    user = await User.create({
+      email: "someone@example.com",
+      password: "password",
+      username: "someone",
+    });
+
+    const users = await User.find();
+    const userId = users[0]._id;
+
+    post = await Post.create({
+      message: "some message",
+      image: imageURL,
+      userId: userId,
+    });
   });
 
-  it("has a message", () => {
-    const post = new Post({ message: "some message" });
+  it("when saved, a post has a message", async () => {
     expect(post.message).toEqual("some message");
   });
 
-  it("can list all posts", async () => {
-    const posts = await Post.find();
-    expect(posts).toEqual([]);
+  // image
+  it("has an image", async () => {
+    expect(post.image).toEqual("exampleImageURLHere");
   });
 
-  it("can save a post", async () => {
-    const post = new Post({ message: "some message" });
+  // dateTime - verified with mongoose timestamps
+  it("has a created date", async () => {
+    expect(post.createdAt).toBeInstanceOf(Date);
+    expect(post.updatedAt).toBeInstanceOf(Date);
+  });
 
-    await post.save();
+  // userId attached to post object
+  it("has a userId attached to post", async () => {
+    expect(mongoose.Types.ObjectId.isValid(post.userId)).toBe(true);
+  });
+
+  it("can list all posts", async () => {
+    await Post.deleteMany({});
     const posts = await Post.find();
-    expect(posts[0].message).toEqual("some message");
+    expect(posts).toEqual([]);
   });
 });
