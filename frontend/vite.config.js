80b8a6b87d5caafc0c741a67f51dc1e3f7fb15d6
Zameer Mohamed <zameermohamed@hotmail.com>
2025-04-02 17:07:46 +0100

    new method vite coverage test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: "jsdom",
+//     coverage: {
+//       provider: "v8", // Make sure this is set correctly
+//       thresholds: {
+//         global: {
+//           branches: 60,
+//           functions: 60,
+//           lines: 60,
+//           statements: 60,
+//         },
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      thresholds: {
+        // Note: 'thresholds' instead of 'threshold'
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
+    },
   },
 });
