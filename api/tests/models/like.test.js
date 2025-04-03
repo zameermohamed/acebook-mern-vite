d3481dce8d6a6c1991b7fc1c62a3c82ae531b1f0
Amritpal <amritpalchahal@hotmail.co.uk>
2025-04-03 18:01:14 +0100

    refactor and logs removed in like model tests

--- api/tests/models/like.test.js
+++ api/tests/models/like.test.js
@@ -5,8 +5,7 @@ const User = require("../../models/user");
 const Like = require("../../models/like");
 
 describe("Like model", () => {
-  let testUser;
-  let testPost;
+  let testUser, testPost, testLike;
 
   beforeEach(async () => {
     await Post.deleteMany({});
@@ -31,25 +30,12 @@ describe("Like model", () => {
   });
 
   it("should allow a user to like a post, with the userId and postId in the Like model matching", async () => {
-    // const like = await Like.create({
-    //   userId: testUser._id,
-    //   postId: testPost._id,
-    // });
-
-    // console.log(like);
-    // console.log("Like userId ->", like.userId);
-    // console.log("Like postId ->", like.postId);
-    // console.log("Like userId to string ->", like.userId.toString());
-    // console.log("Like postId to string->", like.postId.toString());
-
     expect(testLike).toBeDefined();
     expect(testLike.userId).toEqual(testUser._id);
     expect(testLike.postId).toEqual(testPost._id);
   });
 
   it("should not allow a user to like the same post twice", async () => {
-    // await Like.create({ userId: testUser._id, postId: testPost._id });
-
     // Trying to like same post again
     await expect(
       Like.create({ userId: testUser._id, postId: testPost._id })
@@ -57,20 +43,13 @@ describe("Like model", () => {
   });
 
   it("should allow a user to unlike a post", async () => {
-    // const like = await Like.create({
-    //   userId: testUser._id,
-    //   postId: testPost._id,
-    // });
-
-    // console.log(testLike);
-
     await Like.findOneAndDelete({ userId: testUser._id, postId: testPost._id });
+
     const checkLikeDeleted = await Like.findOne({
       userId: testUser._id,
       postId: testPost._id,
     });
 
-    // console.log(checkLikeDeleted);
     expect(checkLikeDeleted).toBeNull();
   });
 
@@ -79,26 +58,24 @@ describe("Like model", () => {
       userId: new mongoose.Types.ObjectId(),
       postId: testPost._id,
     });
-    // console.log("unliking post", checkDifferentUserIdInLikes);
+
     expect(checkDifferentUserIdInLikes).toBeNull();
   });
 
   it("should retrieve all likes for a post", async () => {
     // Creating a second user to like the same post set above in beforeEach
-    testUserTwo = await User.create({
+    const testUserTwo = await User.create({
       email: "someotherone@example.com",
       password: "passWord123!",
       username: "someoneelse",
     });
 
-    testLikeTwo = await Like.create({
+    const testLikeTwo = await Like.create({
       userId: testUserTwo._id,
       postId: testPost._id,
     });
 
     const likes = await Like.find({ postId: testPost._id });
-
-    // console.log(likes);
 
     expect(likes.length).toBe(2);
     expect(likes[0].userId).toEqual(testUser._id);
