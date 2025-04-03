01a70e4d58d64379ff0367ba795e04f6dd4f4c2e
Aysin <aysinn.akpinar@gmail.com>
2025-04-03 16:20:12 +0100

    route/comments and app updated for handling comments

--- api/app.js
+++ api/app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use("/users", usersRouter);
 app.use("/posts", tokenChecker, postsRouter);
 app.use("/tokens", authenticationRouter);
+app.use("/posts/:postId", tokenChecker, commentsRouter);
 
 // 404 Handler
 app.use((_req, res) => {
