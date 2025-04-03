1d8d59f43ffbd8c1f7234d5f9fb09c7858814e1e
Aysin <aysinn.akpinar@gmail.com>
2025-04-03 12:14:09 +0100

    comment model and tests all passing now

--- api/tests/models/comment.test.js
+++ api/tests/models/comment.test.js
@@ -3,79 +3,77 @@ const mongoose = require("mongoose");
 const Post = require("../../models/post");
 const User = require("../../models/user");
 const Comment = require("../../models/comment");
-const imageURL = "exampleImageURLHere";
 
 describe("Comment model", () => {
-    let user;
-    let post;
+    let user1, user2;
+    let post1Id, post2Id;
     let comment;
 
-  beforeEach(async () => {
-    await Post.deleteMany({});
-    await User.deleteMany({});
-    await Comment.deleteMany({});
+    beforeEach(async () => {
+        await Post.deleteMany({});
+        await User.deleteMany({});
+        await Comment.deleteMany({});
 
-    user1 = await User.create({
-        email: "someone@example.com",
-        password: "Password123!",
-        username: "someone",
+        // Create users
+        user1 = await User.create({
+            email: "someone@example.com",
+            password: "Password123!",
+            username: "someone",
+        });
+
+        user2 = await User.create({ 
+            email: "someone1@example.com",
+            password: "Password123!",
+            username: "some_one",
+        });
+
+        // Create posts
+        const postUser1 = await Post.create({
+            message: "User1's post",
+            userId: user1._id, 
+        });
+
+        const postUser2 = await Post.create({
+            message: "User2's post",
+            userId: user2._id, 
+        });
+
+        user1Id = user1._id;
+        user2Id = user2._id;
+        post1Id = postUser1._id;
+        post2Id = postUser2._id;
+
+        // Create a comment
+        comment = await Comment.create({
+            message: "User1 commenting on User2's post",
+            userId: user1Id,
+            postId: post2Id,   
+        });
     });
-    user2 = await User.create({ 
-        email: "someone1@example.com",
-        password: "Password123!",
-        username: "some_one",
+
+    it("when saved, a comment has a message", async () => {
+        expect(comment.message).toEqual("User1 commenting on User2's post");
     });
 
-    const users = await User.find();
-    const user1Id= users[0]._id;
-    const user2Id = users[1]._id;
-    
-    postUser1 = await Post.create({
-        message: "some message",
-        userId: user1Id,
+    it("has a comment attached to a valid post", async () => {
+        expect(mongoose.Types.ObjectId.isValid(comment.postId)).toBe(true);
     });
-    postUser2 = await Post.create({
-        message: "some message",
-        userId: user2Id,
+
+    it("has an associated post", async () => {
+        expect(comment.postId).toEqual(post2Id);
     });
-    const posts = await Post.find();
-    const post1Id = posts[0]._id;
-    const post2Id = posts[1]._id;
 
-
-    comment = await Comment.create({
-        message: "User1 commenting on User2's post",
-        userId: user1Id,
-        postId: post2Id
+    it("has a userId attached to comment", async () => {
+        expect(mongoose.Types.ObjectId.isValid(comment.userId)).toBe(true);
     });
-  });
 
-  it("when saved, a comment has a message", async () => {
-    expect(comment.message).toEqual("User1 commenting on User2's post");
-  });
+    it("has an associated user", async () => {
+        expect(comment.userId).toEqual(user1Id);
+    });
 
-  // image
-  it("has an associated post", async () => {
-    expect(comment.postId).toEqual(post2Id);
-  });
+    // dateTime - verified with mongoose timestamps
+    it("has a created date", async () => {
+        expect(comment.createdAt).toBeInstanceOf(Date);
+    });
 
-  it("has an associated user", async () => {
-    expect(comment.userId).toEqual(user1Id);
-  });
-
-  // dateTime - verified with mongoose timestamps
-  it("has a created date", async () => {
-    expect(comment.createdAt).toBeInstanceOf(Date);
-  });
-
-  // userId attached to post object
-  it("has a userId attached to post", async () => {
-    expect(mongoose.Types.ObjectId.isValid(post.userId)).toBe(true);
-  });
-
-  it("can list all posts", async () => {
-    await Post.deleteMany({});
-    const posts = await Post.find();
-    expect(posts).toEqual([]);
-  });
 });
