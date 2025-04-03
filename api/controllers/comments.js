1bd9f91635af23a1fddac15688a474759fba45eb
Jack Misner <jack.misner@gmail.com>
2025-04-03 18:19:13 +0100

    Started implementing tests. Had to refactor a few routes to get the server to respond properly with the correct data

--- api/controllers/comments.js
+++ api/controllers/comments.js
@@ -1,38 +1,38 @@
 const Comment = require("../models/comment");
-const {decodeToken } = require("../lib/token");
+const { decodeToken } = require("../lib/token");
 
 async function getAllCommentsPerPost(req, res) {
-    const commentsFromPost = await Comment.findOne({ postId: req.params.postId });
-    res.status(200).json({ comments: commentsFromPost });
+  const commentsFromPost = await Comment.find({ postId: req.params.postId });
+  res.status(200).json({ comments: commentsFromPost });
 }
 
 async function createComment(req, res) {
-    try {
-        const postId = req.params.postId;
-        const payload = decodeToken(req.headers.authorization);
-        const userId = payload.user_id;
-        const message = req.body.message;
+  try {
+    const postId = req.params.postId;
+    const payload = decodeToken(req.headers.authorization);
+    const userId = payload.user_id;
+    const message = req.body.message;
 
-        const comment = new Comment({postId, userId, message});
-        await comment.save();
+    const comment = new Comment({ postId, userId, message });
+    await comment.save();
 
-        res.status(201).json({ message: "Comment created" });
-    } catch (err) {
-        console.error("Error caught:", err);
-        //validation errors from comment model
-        if (err.name == 'ValidationError') {
-            const errorMessages = Object.values(err.errors).map(val => val.message);
-            console.log("Validation Error Messages:", errorMessages);
-            return res.status(400).json({ message: errorMessages.join(", ") });
-        }
-        // other errors
-        return res.status(400).json({ message: "Something went wrong" });
+    res.status(201).json({ message: "Comment created" });
+  } catch (err) {
+    console.error("Error caught:", err);
+    //validation errors from comment model
+    if (err.name == "ValidationError") {
+      const errorMessages = Object.values(err.errors).map((val) => val.message);
+      console.log("Validation Error Messages:", errorMessages);
+      return res.status(400).json({ message: errorMessages.join(", ") });
     }
+    // other errors
+    return res.status(400).json({ message: "Something went wrong" });
+  }
 }
 
 const CommentsController = {
-    getAllCommentsPerPost: getAllCommentsPerPost,
-    createComment: createComment
+  getAllCommentsPerPost: getAllCommentsPerPost,
+  createComment: createComment,
 };
 
 module.exports = CommentsController;
