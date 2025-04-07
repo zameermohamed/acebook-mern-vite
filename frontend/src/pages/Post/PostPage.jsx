33122eb673eb6e50fd5f14aa6089bf96225dcb89
Michal <m.podolak14@gmail.com>
2025-04-07 14:50:10 +0100

    feat: add a new comment is now working on single postpage

--- frontend/src/pages/Post/PostPage.jsx
+++ frontend/src/pages/Post/PostPage.jsx
@@ -3,9 +3,17 @@ import { useParams } from "react-router-dom";
 import { getPost } from "../../services/posts";
 import Header from "../../components/Header";
 import Post from "../../components/Post/Post";
+import AddComment from "../../components/AddComment/AddComment";
+import CommentContainer from "../../components/CommentContainer/CommentContainer";
 
 export function PostPage() {
     const [postData, setPostData] = useState();
+    const [commentsData, setCommentsData] = useState([]);
+    const [refreshTrigger, setRefreshTrigger] = useState(0);
+
+    const refreshComments = () => {
+        setRefreshTrigger(refreshTrigger + 1);
+    };
 
     const { id } = useParams();
     useEffect(() => {
@@ -13,21 +21,30 @@ export function PostPage() {
         const loggedIn = token !== null;
         if (loggedIn) {
             getPost(token, id).then((data) => {
-                console.log("from posts page =>", data.postData);
-                setPostData(data);
+                // console.log("from postpage =>", data);
+                console.log("post page =>", data.comments);
+                setPostData(data.postData);
+                setCommentsData(data.comments);
             });
         }
-    }, [id]);
+    }, [id, refreshTrigger]);
 
     return (
         <>
             <Header />
-            <div className="post-container">
-                {postData && (
-                    <Post post={postData.postData} singlePost={true} />
-                )}
+            <div>
+                <div className="post-container">
+                    {postData && <Post post={postData} singlePost={true} />}
+                </div>
+                <div>
+                    <CommentContainer
+                        refreshTrigger={refreshTrigger}
+                        comments={commentsData}
+                    />
+                </div>
+                <AddComment onCommentCreated={refreshComments} />
+                {/* && <h1>{.message}</h1>}</>; */}
             </div>
-            {/* && <h1>{.message}</h1>}</>; */}
         </>
     );
 }
