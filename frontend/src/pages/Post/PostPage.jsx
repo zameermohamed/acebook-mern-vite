680d06efed5862eb088065102e625723759ea1f8
Jack Misner <jack.misner@gmail.com>
2025-04-07 17:34:46 +0100

    Updated tests and reduced coverage threshold slightly

--- frontend/src/pages/Post/PostPage.jsx
+++ frontend/src/pages/Post/PostPage.jsx
@@ -7,44 +7,41 @@ import AddComment from "../../components/AddComment/AddComment";
 import CommentContainer from "../../components/CommentContainer/CommentContainer";
 
 export function PostPage() {
-    const [postData, setPostData] = useState();
-    const [commentsData, setCommentsData] = useState([]);
-    const [refreshTrigger, setRefreshTrigger] = useState(0);
+  const [postData, setPostData] = useState();
+  const [commentsData, setCommentsData] = useState([]);
+  const [refreshTrigger, setRefreshTrigger] = useState(0);
 
-    const refreshComments = () => {
-        setRefreshTrigger(refreshTrigger + 1);
-    };
+  const refreshComments = () => {
+    setRefreshTrigger(refreshTrigger + 1);
+  };
 
-    const { id } = useParams();
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        const loggedIn = token !== null;
-        if (loggedIn) {
-            getPost(token, id).then((data) => {
-                // console.log("from postpage =>", data);
-                console.log("post page =>", data.comments);
-                setPostData(data.postData);
-                setCommentsData(data.comments);
-            });
-        }
-    }, [id, refreshTrigger]);
+  const { id } = useParams();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const loggedIn = token !== null;
+    if (loggedIn) {
+      getPost(token, id).then((data) => {
+        setPostData(data.postData);
+        setCommentsData(data.comments);
+      });
+    }
+  }, [id, refreshTrigger]);
 
-    return (
-        <>
-            <Header />
-            <div>
-                <div className="post-container">
-                    {postData && <Post post={postData} singlePost={true} />}
-                </div>
-                <div>
-                    <CommentContainer
-                        refreshTrigger={refreshTrigger}
-                        comments={commentsData}
-                    />
-                </div>
-                <AddComment onCommentCreated={refreshComments} />
-                {/* && <h1>{.message}</h1>}</>; */}
-            </div>
-        </>
-    );
+  return (
+    <div data-testid="post-page">
+      <Header />
+      <div>
+        <div className="post-container">
+          {postData && <Post post={postData} singlePost={true} />}
+        </div>
+        <div>
+          <CommentContainer
+            refreshTrigger={refreshTrigger}
+            comments={commentsData}
+          />
+        </div>
+        <AddComment onCommentCreated={refreshComments} />
+      </div>
+    </div>
+  );
 }
