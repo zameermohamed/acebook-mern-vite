858cc776991e5df6509ae9ad21d3ccf3a3f417b0
Michal <m.podolak14@gmail.com>
2025-04-07 11:01:53 +0100

    feat: display post component on post page

--- frontend/src/pages/Post/PostPage.jsx
+++ frontend/src/pages/Post/PostPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getPost } from "../../services/posts";
 import Header from "../../components/Header";
+import Post from "../../components/Post/Post";
 
 export function PostPage() {
     const [postData, setPostData] = useState();
@@ -12,13 +13,21 @@ export function PostPage() {
         const loggedIn = token !== null;
         if (loggedIn) {
             getPost(token, id).then((data) => {
-                console.log(data.postData.message);
+                console.log("from posts page =>", data.postData);
                 setPostData(data);
             });
         }
     }, [id]);
 
-    return <>
-    <Header />
-    {postData && <h1>{postData.postData.message}</h1>}</>;
+    return (
+        <>
+            <Header />
+            <div className="post-container">
+                {postData && (
+                    <Post post={postData.postData} singlePost={true} />
+                )}
+            </div>
+            {/* && <h1>{.message}</h1>}</>; */}
+        </>
+    );
 }
