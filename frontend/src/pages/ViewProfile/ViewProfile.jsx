7a05ad912be7c2f5d09c0f49d3e71d0ef3105455
Alec McGill <alec.mcgill@yahoo.co.uk>
2025-04-07 11:38:20 +0100

    beginning the rendering of the page to show another user's profile. Having issues fetching the user data

--- frontend/src/pages/ViewProfile/ViewProfile.jsx
+++ frontend/src/pages/ViewProfile/ViewProfile.jsx
@@ -3,3 +3,41 @@ import { useParams } from "react-router-dom";
 import { getOtherUser } from "../../services/users";
 import Header from "../../components/Header";
 
+export function ViewProfile() {
+    const [user, setUser] = useState(null);
+    const { username } = useParams();
+    const formatDate = (date) => {
+        let dateFormat = new Date(date);
+
+        // Options for formatting
+        let options = {
+            month: "long",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false,
+        };
+        let formattedDate = dateFormat.toLocaleString("en-US", options);
+        return formattedDate;
+    };
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        async function fetchUser() {
+            try {
+            const userData = await getOtherUser(token, username);
+            setUser(userData)
+            console.log(userData)
+            } catch (error) {
+                console.error("failed to find user", error)
+            } 
+        }
+
+        fetchUser();
+        }, [username]);
+
+    return (
+        <Header />
+    )
+}
