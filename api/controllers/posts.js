2cfd863f3fe15fba01cdf07516842e64c0f426a9
Alec McGill <alec.mcgill@yahoo.co.uk>
2025-04-08 10:38:56 +0100

    creates post container for individual profiles

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,35 +1,50 @@
 const Post = require("../models/post");
+const User = require("../models/user");
 const { generateToken } = require("../lib/token");
 
 async function getAllPosts(req, res) {
-    const posts = await Post.find().populate("userId");
+  const posts = await Post.find().populate("userId");
+  const token = generateToken(req.user_id);
+  res.status(200).json({ posts: posts, token: token });
+}
 
-    const token = generateToken(req.user_id);
-    res.status(200).json({ posts: posts, token: token });
+async function getPostsByUser(req, res) {
+  const username = req.params.username; 
+  try {
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const foundPosts = await Post.find({ userId: user._id }).populate("userId");
+    res.status(200).json({ posts: foundPosts })
+  } catch(err) {
+    console.error(err)
+  }
 }
 
 async function getPost(req, res, next) {
-    const postId = req.params.id;
-    try {
-        const foundPost = await Post.findOne({ _id: postId });
-        req.postData = foundPost;
-        next();
-    } catch (err) {
-        return res.status(400).json({ message: "Invalid post ID format" });
-    }
+  const postId = req.params.id;
+  try {
+    const foundPost = await Post.findOne({ _id: postId });
+    req.postData = foundPost;
+    next();
+  } catch (err) {
+    return res.status(400).json({ message: "Invalid post ID format" });
+  }
 }
 
 async function createPost(req, res) {
-    const post = new Post(req.body);
-    post.save();
-    const newToken = generateToken(req.user_id);
-    res.status(201).json({ message: "Post created", token: newToken });
+  const post = new Post(req.body);
+  post.save();
+  const newToken = generateToken(req.user_id);
+  res.status(201).json({ message: "Post created", token: newToken });
 }
 
 const PostsController = {
-    getAllPosts: getAllPosts,
-    getPost: getPost,
-    createPost: createPost,
+  getAllPosts: getAllPosts,
+  getPost: getPost,
+  createPost: createPost,
+  getPostsByUser: getPostsByUser
 };
 
 module.exports = PostsController;
--- frontend/src/pages/ViewProfile/ViewProfile.jsx
+++ frontend/src/pages/ViewProfile/ViewProfile.jsx
@@ -1,43 +1,46 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getOtherUser } from "../../services/users";
+import { getPostsByUser } from "../../services/posts"
 import Header from "../../components/Header";
+import ProfilePostContainer from "../../components/ProfilePostContainer";
 
 export function ViewProfile() {
-    const [user, setUser] = useState(null);
-    const { username } = useParams();
-    const formatDate = (date) => {
-        let dateFormat = new Date(date);
-
-        // Options for formatting
-        let options = {
-            month: "long",
-            day: "numeric",
-            year: "numeric",
-        };
-        let formattedDate = dateFormat.toLocaleString("en-US", options);
-        return formattedDate;
+  const [user, setUser] = useState(null);
+  const { username } = useParams();
+  const formatDate = (date) => {
+    let dateFormat = new Date(date);
+    // Options for formatting
+    let options = {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
     };
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        const loggedIn = token !== null;
-        if (loggedIn) {
-            getOtherUser(token, username).then((data) => {
-                setUser(data);
-            });
-        }
-    }, [username]);
+    let formattedDate = dateFormat.toLocaleString("en-US", options);
+    return formattedDate;
+    };
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const loggedIn = token !== null;
+    if (loggedIn) {
+      getOtherUser(token, username).then((data) => {
+        setUser(data);
+      });
+      getPostsByUser(username)
+    }
+  }, [username]);
 
-    return (
-        <>
-          <div>
-            <Header></Header>
-          </div>
-          {user && (<div className="profile-page">
-            <h1>{user.username}</h1>
-            {user.profilePicture && (<img src={user.profilePicture}/>)}
-            <p> User since: {formatDate(user.dateCreated)}</p>
-          </div>)}
-        </>
-      );
+  return (
+    <>
+      <div>
+        <Header></Header>
+      </div>
+      {user && (<div className="profile-page">
+        <h1>{user.username}</h1>
+        {user.profilePicture && (<img src={user.profilePicture}/>)}
+        <p> User since: {formatDate(user.dateCreated)}</p>
+        <ProfilePostContainer />
+      </div>)}
+    </>
+  );
 }
