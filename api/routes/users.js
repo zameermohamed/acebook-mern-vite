91fcfa78c44234cdd24b187ea74251a315b80676
Aysin <aysinn.akpinar@gmail.com>
2025-04-08 09:48:15 +0100

    edit user backend

--- api/routes/users.js
+++ api/routes/users.js
@@ -8,5 +8,9 @@ const router = express.Router();
 router.post("/", UsersController.create);
 // MICHAL & ALEC - add token checker to get user_id on request
 router.get("/", tokenChecker, UsersController.getUser);
+// AYSIN - add token checker to update the user
+router.put("/", tokenChecker, UsersController.updateUser);
+// AYSIN - add token checker to delete the user
+router.delete("/", tokenChecker, UsersController.deleteUser);
 
 module.exports = router;
