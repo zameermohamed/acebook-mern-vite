6ff0755148811f7c70a6c3a26174c26f6cd9b4c5
Alec McGill <alec.mcgill@yahoo.co.uk>
2025-04-09 17:45:56 +0100

    sorts the tests out

--- frontend/tests/pages/viewProfilePage.test.jsx
+++ frontend/tests/pages/viewProfilePage.test.jsx
@@ -1,12 +1,21 @@
 import { ViewProfile } from "../../src/pages/ViewProfile/ViewProfile";
 import { render, screen } from "@testing-library/react";
 import { vi } from "vitest";
+import { getUser } from "../../src/services/users";
 import { getPostsByUser } from "../../src/services/posts";
+import { getPosts } from "../../src/services/posts";
 import { MemoryRouter } from "react-router-dom";
+import '@testing-library/jest-dom';
 
-vi.mock("../../src/services/posts", () => {
+  vi.mock("../../src/services/users", () => {
+    const getUserMock = vi.fn();
+    return { getUser: getUserMock };
+  });
+
+  vi.mock("../../src/services/posts", () => {
     const getPostsByUserMock = vi.fn();
-    return { getPostsByUser: getPostsByUserMock };
+    const getPostsMock = vi.fn();
+    return { getPostsByUser: getPostsByUserMock, getPosts: getPostsMock};
   });
 
 describe("View another user's profile", () => {
@@ -16,21 +25,28 @@ describe("View another user's profile", () => {
 
   test("user's profile picture, username, and date of creation render if no posts", async () => {
     window.localStorage.setItem("token", "testToken");
-    const mockUser = [
-      { username: "testUser", dateCreated: "2025-04-07T14:10:05.266+00:00", profilePicture: "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg", email: "testEmail", password: "1!Acebook" },
-      ];
-        
+    const mockUser =
+      { username: "testUser", dateCreated: "2025-04-07T14:10:05.266+00:00", profilePicture: "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg", email: "testEmail", password: "1!Acebook" };
+
+      const mockUser2 = 
+      { username: "testUser2", dateCreated: "2025-04-07T14:10:05.266+00:00", profilePicture: "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg", email: "testEmail2", password: "2!Acebook" };        
+      
+      getUser.mockResolvedValue(mockUser);
+
       getPostsByUser.mockResolvedValue({
-        foundUser: mockUser,
-        token: "testToken"
+        foundUser: mockUser2
       })
+      
+      getPosts.mockResolvedValue([]);
+
       render(
         <MemoryRouter>
           <ViewProfile />
         </MemoryRouter>
       );
 
-      const usernameEl = await screen.findByTestId("username")
-      expect(document).toContain(usernameEl)
+      const usernameEl = await screen.findByTestId("username");
+      expect(usernameEl).toBeInTheDocument();
+      expect(usernameEl).toHaveTextContent("testUser2");
     })
 })
