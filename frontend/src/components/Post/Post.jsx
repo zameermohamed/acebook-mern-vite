5ce9836ca1e6ccb3c728839e1f4364c661d53d5e
Michal <m.podolak14@gmail.com>
2025-04-10 11:46:30 +0100

    fix: delete post updates post page

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,107 +1,123 @@
 import "../Post/Post.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import LikeComponent from "../LikeComponent/LikeComponent";
 import { useEffect, useState } from "react";
 import { getUser } from "../../services/users";
 import { deletePost } from "../../services/posts";
 
 function Post(props) {
-  const [userId, setUserId] = useState();
-  const token = localStorage.getItem("token");
-  useEffect(() => {
-    getUser(token).then((data) => {
-      setUserId(data.userId);
-    });
-  }, []);
+    const [userId, setUserId] = useState();
+    const token = localStorage.getItem("token");
+    const navigate = useNavigate();
 
-  const formatDate = (date) => {
-    let dateFormat = new Date(date);
+    useEffect(() => {
+        getUser(token).then((data) => {
+            setUserId(data.userId);
+        });
+    }, [token]);
 
-    // Options for formatting
-    let options = {
-      month: "long",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false,
+    const formatDate = (date) => {
+        let dateFormat = new Date(date);
+
+        // Options for formatting
+        let options = {
+            month: "long",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false,
+        };
+
+        let formattedDate = dateFormat.toLocaleString("en-US", options);
+        return formattedDate;
     };
 
-    let formattedDate = dateFormat.toLocaleString("en-US", options);
-    return formattedDate;
-  };
+    const handlePostClick = (e) => {
+        // Don't navigate if the click was on the like component
+        if (e.target.closest(".like-component")) {
+            e.preventDefault();
+        }
+        if (e.target.closest(".bin-container")) {
+            e.preventDefault();
+        }
+    };
 
-  const handlePostClick = (e) => {
-    // Don't navigate if the click was on the like component
-    if (e.target.closest(".like-component")) {
-      e.preventDefault();
-    }
-  };
+    const handlePostDelete = async () => {
+        console.log("handleDelete");
+        await deletePost(token, props.post._id);
+        if (props.onPostDeleted) props.onPostDeleted();
+        navigate("/posts");
+    };
 
-  const handlePostDelete = () => {
-    deletePost(token, props.post._id);
-    if (props.onPostDeleted) props.onPostDeleted();
-  };
+    // Get the comment count from props.comments (when in single post view)
+    // or from post.commentsCount (when in feed view)
+    const commentCount =
+        props.comments?.length || props.post.commentsCount || 0;
 
-  // Get the comment count from props.comments (when in single post view)
-  // or from post.commentsCount (when in feed view)
-  const commentCount = props.comments?.length || props.post.commentsCount || 0;
+    return (
+        <>
+            {props.post.userId && (
+                <Link
+                    className="post-card"
+                    key={props.post._id}
+                    to={`/posts/${props.post._id}`}
+                    onClick={handlePostClick}
+                >
+                    <p className="post-card-date">
+                        {formatDate(props.post.createdAt)}
+                    </p>
+                    <div className="post-card-user-info">
+                        <div className="user-details">
+                            <Link to={`/users/${props.post.userId.username}`}>
+                                <img
+                                    className="post-card-user-picture"
+                                    src={props.post.userId.profilePicture}
+                                    alt="User profile"
+                                />
+                            </Link>
+                            <div className="post-card-user-name">
+                                <Link
+                                    to={`/users/${props.post.userId.username}`}
+                                >
+                                    {props.post.userId.username}
+                                </Link>
+                            </div>
+                        </div>
 
-  return (
-    <>
-      {props.post.userId && (
-        <Link
-          className="post-card"
-          key={props.post._id}
-          to={`/posts/${props.post._id}`}
-          onClick={handlePostClick}
-        >
-          <p className="post-card-date">{formatDate(props.post.createdAt)}</p>
-          <div className="post-card-user-info">
-            <div className="user-details">
-              <Link to={`/users/${props.post.userId.username}`}>
-                <img
-                  className="post-card-user-picture"
-                  src={props.post.userId.profilePicture}
-                  alt="User profile"
-                />
-              </Link>
-              <div className="post-card-user-name">
-                <Link to={`/users/${props.post.userId.username}`}>
-                  {props.post.userId.username}
+                        {userId === props.post.userId._id && (
+                            <div
+                                className="bin-container"
+                                onClick={handlePostDelete}
+                            >
+                                <img
+                                    className="bin-icon"
+                                    src="/src/images/bin.png"
+                                    alt="Delete"
+                                />
+                            </div>
+                        )}
+                    </div>
+
+                    <p data-testid="post-message" className="post-message">
+                        {props.post.message}
+                    </p>
+                    {(props.post.likesCount === 0 || props.post.likesCount) && (
+                        <LikeComponent
+                            likesCount={props.post.likesCount}
+                            postId={props.post._id}
+                            userHasLiked={props.post.userHasLiked}
+                            theme={props.theme}
+                        />
+                    )}
+                    <p className="post-comments-count">
+                        {commentCount}{" "}
+                        {commentCount === 1 ? "Comment" : "Comments"}
+                    </p>
                 </Link>
-              </div>
-            </div>
-
-            {userId === props.post.userId._id && (
-              <Link onClick={handlePostDelete} to="/posts">
-                <img
-                  className="bin-icon"
-                  src="/src/images/bin.png"
-                  alt="Delete"
-                />
-              </Link>
             )}
-          </div>
-
-          <p data-testid="post-message" className="post-message">
-            {props.post.message}
-          </p>
-          {(props.post.likesCount === 0 || props.post.likesCount) && (
-            <LikeComponent
-              likesCount={props.post.likesCount}
-              postId={props.post._id}
-              userHasLiked={props.post.userHasLiked}
-              theme={props.theme}
-            />
-          )}
-          <p className="post-comments-count">
-            {commentCount} {commentCount === 1 ? "Comment" : "Comments"}
-          </p>
-        </Link>
-      )}
-    </>
-  );
+        </>
+    );
 }
 
 export default Post;
