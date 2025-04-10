84b1627bd2f28f6f17a9a37074972e9d0e428699
Michal <m.podolak14@gmail.com>
2025-04-10 20:24:50 +0100

    update: post picture size update

--- api/app.js
+++ api/app.js
@@ -15,7 +15,7 @@ const app = express();
 // docs: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 // docs: https://expressjs.com/en/resources/middleware/cors.html
 app.use(cors());
-
+app.use(express.json({ limit: "14mb" }));
 // Parse JSON request bodies, made available on `req.body`
 app.use(bodyParser.json());
 
@@ -28,17 +28,17 @@ app.use("/tokens", authenticationRouter);
 
 // 404 Handler
 app.use((_req, res) => {
-  res.status(404).json({ err: "Error 404: Not Found" });
+    res.status(404).json({ err: "Error 404: Not Found" });
 });
 
 // Error handler
 app.use((err, _req, res, _next) => {
-  console.error(err);
-  if (process.env.NODE_ENV === "development") {
-    res.status(500).send(err.message);
-  } else {
-    res.status(500).json({ err: "Something went wrong" });
-  }
+    console.error(err);
+    if (process.env.NODE_ENV === "development") {
+        res.status(500).send(err.message);
+    } else {
+        res.status(500).json({ err: "Something went wrong" });
+    }
 });
 
 module.exports = app;
